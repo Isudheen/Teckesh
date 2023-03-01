947c662229c9e8f1c1920593ae616b5865821d62
Isudheen <61105586+Isudheen@users.noreply.github.com>
2023-03-01 17:43:41 +0530

    initial-commit

---
+++ src/app/pages/home/components/filters/filters.component.ts
@@ -0,0 +1,16 @@
+import { Component, Output, EventEmitter } from '@angular/core';
+
+@Component({
+  selector: 'app-filters',
+  templateUrl: './filters.component.html',
+  styleUrls: ['./filters.component.css'],
+})
+export class FiltersComponent {
+  @Output() showCategory = new EventEmitter<string>();
+
+  categories = ['shoes', 'sports'];
+
+  onShowCategory(category: string): void {
+    this.showCategory.emit(category);
+  }
+}
